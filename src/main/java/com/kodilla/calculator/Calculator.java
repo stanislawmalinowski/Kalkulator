af3a82019dc50490bec73bffa411566a0727e9da
Stanislaw Malinowski <s.malinowski197@wp.pl>
2023-02-07 18:53:17 +0100

    First calculator

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.calculator;
+
+import java.sql.SQLOutput;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a + b );
+        System.out.println(b - a);
+
+
+    }
+
+}
